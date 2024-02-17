0c27c5a4d93016a95f8dde5990cdc407a730472e
shiv42x <shivy@SHIVAM-PC>
2024-02-17 17:26:22 -0500

    Add project files.

---
+++ EntityManager.cpp
@@ -0,0 +1,15 @@
+#include <memory>
+
+class Entity
+{
+public:
+
+	/*
+	std::shared_ptr<CTransform> cTransform;
+	std::shared_ptr<CName> cName;
+	std::shared_ptr<CShape> cShape;
+	std::shared_ptr<CBBox> cBBox;
+	*/
+	Entity() {}
+
+};
